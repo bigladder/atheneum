8288b89de0a2df2afd89ffa1f2d6cae381f1fe42
Neal Kruis <neal.kruis@bigladdersoftware.com>
2023-02-22 16:24:48 -0700

    Add logging base class and private class for interface.

--- include/atheneum/atheneum.h
+++ include/atheneum/atheneum.h
@@ -3,11 +3,26 @@
 
 #pragma once
 
+#include <memory>
+#include <functional>
+#include <string_view>
+
 namespace Atheneum {
 
+enum class MessageLevel;
+using LoggingCallbackFunction = std::function<void(MessageLevel, const std::string_view, void*)>;
+
+class AtheneumPrivate;
+
 class Atheneum {
-public:
-  int answer();
+  public:
+    Atheneum(LoggingCallbackFunction logging_callback_function_ = nullptr,
+             void* logging_callback_context_ = nullptr);
+    ~Atheneum();
+    int answer();
+
+  private:
+    std::unique_ptr<AtheneumPrivate> atheneum;
 };
 
 } // namespace Atheneum
--- src/atheneum.cpp
+++ src/atheneum.cpp
@@ -1,10 +1,43 @@
 /* Copyright (c) 2019 Big Ladder Software LLC. All rights reserved.
  * See the LICENSE file for additional terms and conditions. */
 
+#include <memory>
+
+#include <fmt/format.h>
+
 #include <atheneum/atheneum.h>
+
+#include "atheneum-private.h"
+#include "logging-base.h"
 
 namespace Atheneum {
 
-int Atheneum::answer() { return 42; }
+Atheneum::Atheneum(LoggingCallbackFunction logging_callback_function_,
+                   void* logging_callback_context_)
+{
+    atheneum = std::unique_ptr<AtheneumPrivate>(
+        new AtheneumPrivate(logging_callback_function_, logging_callback_context_));
+}
+
+Atheneum::~Atheneum() {}
+
+int Atheneum::answer() { return atheneum->answer(); }
+
+AtheneumPrivate::AtheneumPrivate(LoggingCallbackFunction logging_callback_function_,
+                                 void* logging_callback_context_)
+    : LoggingBase(logging_callback_function_, logging_callback_context_)
+{
+}
+
+int AtheneumPrivate::answer()
+{
+    int answer = 42;
+    log_message(
+        MessageLevel::MESSAGE_INFO,
+        fmt::format(
+            "The answer to the ultimate question of life, the universe, and everything is: {}.",
+            answer));
+    return answer;
+}
 
 } // namespace Atheneum
