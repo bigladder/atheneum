56d6198ad12f40d54fd328afd4e1a97ed42c6cdf
Phil Ahrenkiel <spahrenk@gmail.com>
2024-07-01 14:48:54 -0600

    Allow different submodule folder name.

--- cmake/initialize-submodules.cmake
+++ cmake/initialize-submodules.cmake
@@ -1,17 +1,17 @@
 #update the named submodule
-macro(update_submodule module_name)
-  if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${module_name}")
-    message(FATAL_ERROR "Submodule directory \"${CMAKE_CURRENT_SOURCE_DIR}/${module_name}\" does not exist")
+macro(update_submodule module_name module_path)
+  if(NOT EXISTS "${module_path}")
+    message(FATAL_ERROR "Submodule directory \"${module_path}\" does not exist")
   endif()
 
   # Initialize submodule if it hasn't already been cloned
   if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${module_name}/.git")
     message(STATUS "Initialize ${module_name} submodule")
-    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init "${CMAKE_CURRENT_SOURCE_DIR}/${module_name}"
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init "${module_path}"
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             RESULT_VARIABLE GIT_SUBMOD_RESULT)
     if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-      message(FATAL_ERROR "git submodule update --init ${CMAKE_CURRENT_SOURCE_DIR}/${module_name} failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+      message(FATAL_ERROR "git submodule update --init ${module_path} failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
     endif()
   endif()
 
@@ -31,24 +31,19 @@ endmacro()
 #update the named submodule iff it is listed in .gitmodules
 macro(add_submodule module_name)
 
-  if (NOT TARGET ${module_name}) # Don't clone or add subdirectory if the module/target already exists
-    update_submodule(${module_name}) # Maybe this could be expanded in this macro instead of being a separate macro?
-    add_subdirectory(${module_name})
+  #check of specific path is provided
+  set(MacroArgs ${ARGN})
+  list(LENGTH MacroArgs NumArgs)
+  if(NumArgs GREATER 0)
+    set(module_path ${ARGV1})
+  else()
+    set(module_path ${CMAKE_CURRENT_SOURCE_DIR}/${module_name})
+  endif()
+  message(${module_path})
+
+  if (NOT TARGET ${module_name})
+    update_submodule(${module_name} ${module_path})
+    add_subdirectory(${module_path})
   endif()
 
-  set (extra_args ${ARGN})
-  list(LENGTH extra_args extra_count)
-  if (${extra_count} GREATER 0)
-    list(GET extra_args 0 optional_arg)
-    message ("Got an optional arg: ${optional_arg}")
-  endif ()
-
-endmacro()
-
-#update the named submodule iff it is listed in .gitmodules
-macro(add_submodules)
-  set (modules ${ARGN})
-  foreach(module ${modules})
-    add_submodule(${module})
-  endforeach()
 endmacro()
